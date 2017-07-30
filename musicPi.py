9c6bc744feca48280eec324246b95d11af33db4e
Moritz <phis@mogobb.de>
2017-07-30 13:18:59 +0200

    Added alarm and percentage display

--- musicPi.py
+++ musicPi.py
@@ -15,6 +15,22 @@ def get_artist_and_title():
 	
 	return artist, title
 
+def get_progress():
+	statusinfo = client.status()
+	
+	if statusinfo['state'] == 'stop':
+		return 0
+	
+	elapsed, total = statusinfo['time'].split(':')
+	elapsed = int(elapsed)
+	total = int(total)
+	
+	pct = 0
+	if total > 0:
+		pct = round(elapsed/total, 2)
+	
+	return min(pct, 100)
+	
 def get_time_and_date():
 	current_time = time.strftime("%H:%M")
 	current_date = time.strftime("%d-%m")
@@ -39,29 +55,31 @@ def get_rgb(str):
 		b = random.randrange(255)	
 	return r, g, b
 	
-# def alarm_check(schedule):
-	# while running:
-		# if alarm_status:
-			# current_time = time.localtime()
-			# # current_hour_and_minute = time.strftime("%H:%M", current_time)
-			# current_hour_and_minute = "09:42"
-			# current_weekday = time.strftime("%A", current_time)
-			# if schedule[current_weekday] == current_hour_and_minute:
-				# alarm()
-		# time.sleep(1)
+def alarm_check():
+	schedule = {'Monday': "09:42", 'Tuesday': "07:00", 'Wednesday': "07:00", 'Thursday': "07:00", 'Friday': "07:00", 'Saturday': "07:00", 'Sunday': "13:10"}
+	current_time = time.strftime("%H:%M")
+	current_weekday = time.strftime("%A")
+	
+	if schedule[current_weekday] == current_time:
+		alarm()
 		
-# def alarm():
-	# write_at_position("ALARM!", 0, 1)
+def alarm():
+	# TODO: load alarm playlist
+	client.shuffle()
+	client.play()
 
-def clear_display(line=4):
+# cleans either a single line or all lines on the display
+def clear_display(line=3):
 	display_lock.acquire(timeout=1)
-	if line > 3:
+	if line > 2:
 		lcd.clear()
 	else:
 		lcd.set_cursor_position(0,line)
 		lcd.write(16*" ")
 	display_lock.release()
 
+# writes a string at a given position on the display
+# doesn't prohibit line breaks
 def write_at_position(str, x, y):
 	display_lock.acquire(timeout=1)
 	if x < 16 and y < 3:
@@ -69,15 +87,17 @@ def write_at_position(str, x, y):
 		lcd.write(str)
 	display_lock.release()
 
-# music in second line
+# music in first line
 def music_display():
 	global last_artist, last_song
 	while running:
 		current_artist, current_song = get_artist_and_title()
+		progress = get_progress()
+		backlight.set_graph(progress)
 		if last_artist != current_artist or last_song != current_song:
 			last_artist, last_song = current_artist, current_song
 			
-			now_playing = current_artist + '-' + current_song
+			now_playing = current_artist + ' - ' + current_song
 			
 			if len(now_playing) > 16:
 				now_playing = now_playing[0:15]
@@ -86,8 +106,12 @@ def music_display():
 			
 			write_at_position(now_playing, 0, 0)
 			
-			client.idle()
-	
+			r, g, b = get_rgb(current_artist)
+			backlight.rgb(r,g,b)
+			
+		# client.idle()
+		time.sleep(1)
+			
 # weather in second line
 def weather_display():
 	global last_temp, last_cond
@@ -99,13 +123,11 @@ def weather_display():
 			
 			write_at_position(current_temp, 7-len(current_temp), 1)
 			write_at_position(current_cond, 9, 1)
-
-			r, g, b = get_rgb(current_cond)
-			backlight.rgb(r,g,b)
 			
 			time.sleep(5)
 		
-# date and time in third line		
+# date and time in third line
+# also calls alarm_check()
 def time_display():
 	global last_time, last_date
 	while running:
@@ -117,6 +139,7 @@ def time_display():
 			
 			if alarm_status:
 				write_at_position(".", 1, 2)
+				alarm_check()
 			else:
 				write_at_position(" ", 1, 2)
 			write_at_position(current_time, 2, 2)
@@ -135,9 +158,7 @@ try:
 	client.connect("localhost", 6600)
 
 	running = True
-	alarm_status = False
-	
-	alarm_dict = {'Monday': "09:42", 'Tuesday': "07:00", 'Wednesday': "07:00", 'Thursday': "07:00", 'Friday': "07:00", 'Saturday': "07:00", 'Sunday': "07:00"}
+	alarm_status = True
 	
 	display_lock = threading.Lock()
 	
@@ -153,10 +174,6 @@ try:
 	time_display_thread.setDaemon(True)
 	time_display_thread.start()
 	
-	# alarm_thread = threading.Thread(target=alarm_check,args=(alarm_dict,))
-	# alarm_thread.setDaemon(True)
-	# alarm_thread.start()
-	
 	while True:
 		pass
 	
@@ -165,8 +182,9 @@ except KeyboardInterrupt:
 	music_display_thread.join()
 	weather_display_thread.join()
 	time_display_thread.join()
-	# alarm_thread.join()
 	lcd.clear()
 	backlight.off()
+	client.stop()
+	client.close()
 	print("Ende")
 	
